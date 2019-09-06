93dcc1c038fe2d59d273c568bdcba7725659362f
ZKnobrega <54561781+ZKnobrega@users.noreply.github.com>
2019-09-06 12:29:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,14 +23,10 @@ int main()
 	no *aux; //usado em excluir e pesquisar e inserir por posição.
 	int op;
 	int cont = 0;
+
 inicio:
-	cout << "Escolha a opção desejada: \n1 - Inserir." << endl;
-	cout << "2 - Excluir." << endl;
-	cout << "3 - Listar." << endl;
-	cout << "4 - limpar." << endl;
-	cout << "5 - Sair." << endl;
-	cout << "6 - Inserir por Posição." << endl;
-	cout << "Opção : ";
+	cout << "Escolha a opção desejada: \n1 - Inserir.\n2 - Excluir.\n3 - Listar.\n4 - limpar.\n5 - Sair.\n6 - Inserir por Posição." << endl;
+	cout << "Escolha: ";
 	cin >> op;
 	switch (op)
 	{
@@ -42,48 +38,62 @@ inicio:
 				cont++;
 			} while (aux != NULL);
 
+
 	case 1:
 		//inserir;
 
 		cout << "Digite um valor inteiro para inserir na lista: ";
 		cin >> num;
-		novo = (no *)malloc(sizeof(no)); //O ponteiro 'novo' recebe o local indicado por malloc(); do tamanho definido por sizeof();
-		novo -> valor = num;
-		novo -> prox = lista; //Lista inicialmente vale NULL, depois de ser inserido algum valor 'lista' vai ser igual ao ptr anterior
-		lista = novo;
+		novo = new no; //O ponteiro 'novo' recebe o local dinâmicamente indicado por NEW com o tamanho de espaço para variavel do tipo no.
+		novo -> valor = num; // 'novo' já com o local de memoria aloocado apontando para o valor que irá receber o numero informado pelo usuario
+		novo -> prox = lista; //Lista inicialmente vale NULL, depois de ser inserido algum valor 'lista' vai ser igual ao ptr anterior sendo assim "liata" sempre será o ultimo valor incrementado na lista apontando para o seu proximo torndando 'lista' a cabeça.6
+		lista = novo; // para não perdermos o valor de referencia da lista.
 		cout << "\n\n";
 		goto inicio;
 
 
 	case 2:
 		//excluir;
-		int ind;
-		no *morta;
+		int ind; // será a posição da variavel a ser excluida.
+		no *morta; // o ponteiro que irá retirar a variavel da lista.
 		aux = novo;
-		cout << "Digite o índice do valor a ser excluido: ";
-		cin >> ind;
-        if (ind !=1&& ind <= cont)
+		cout << "A lista é:\n";
+        cont = 0;
+		do
         {
+				cout << cont + 1 << "º " << aux->valor << endl;
+				aux = aux->prox; //O ptr 'aux' agora vai apontar para o proximo valor
+				cont++;
+        }while (aux != NULL);
+        cout << "\nDigite a posição do valor a ser excluido: "<< endl;
+		cin >> ind;
+		aux = novo;
+        if (ind != 1 && ind <= cont)
+        {
+
             for (int i = 0; i < ind - 2; i++)
             {
                 aux = aux->prox;
             }
+
+
             morta = aux->prox;
             aux->prox = morta->prox;
             cout << "\n\n";
         }
-        else if (ind == 1)
+        if (ind == 1)
         {
             novo = aux -> prox;
         }
-        else
+        if (ind < 1 | ind > cont)
         {
             cout << "Opção Invalida!!\n";
         }
 
 		goto inicio;
-
 		break;
+
+
 	case 3:
 		//listar
 
@@ -93,8 +103,7 @@ inicio:
 		}
 		else
 		{
-			cout << "\n\n"
-				 << endl;
+			cout << "\n\n" << endl << "\nA lista é:" << endl;
 			aux = lista;
 			int cont = 0;
 			do
@@ -104,7 +113,7 @@ inicio:
 				cont++;
 			} while (aux != NULL);
 			cout << "\n\n";
-			cout << "A lista possui " << cont << " valores." << endl;
+			cout << "A lista possui " << cont << " Valores." << endl;
 		}
 		cout << "\n\n";
 		goto inicio;
@@ -138,26 +147,22 @@ inicio:
         int posicao;
         cout << "Informe a posição a ser inserida: \n";
         cin >> posicao;
-        novo = (no *)malloc(sizeof(no)); //O ponteiro 'novo' recebe o local indicado por malloc(); do tamanho definido por sizeof();
+        novo = new no; //O ponteiro 'novo' recebe o local indicado por malloc(); do tamanho definido por sizeof();
 		novo->valor = num;
 
         if (posicao > 1 && posicao <= cont)
         {
             aux = lista;
             anterior = lista;
-                for (int i=0; i<posicao; ++i)
+                for (int i=0; i<posicao-1; ++i)
                 {
                     aux = aux -> prox;
                 }
 
-           // cout << aux->valor << endl;
-
-
-            for (int i=0; i<posicao-1; ++i)
+            for (int i=0; i<posicao-2; ++i)
                 {
                     anterior = anterior -> prox;
                 }
-            // cout << anterior->valor << endl;
 
             anterior -> prox = novo;
 
@@ -171,9 +176,6 @@ inicio:
                 novo -> prox = aux;
             }
 
-
-            //novo->prox = lista; //Lista inicialmente vale NULL, depois de ser inserido algum valor 'lista' vai ser igual ao ptr anterior
-            //lista = novo;
         }
 
         else if(posicao == 1){
